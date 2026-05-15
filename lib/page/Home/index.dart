d8a0b0fbe3f9f94ca26a18e87fc44931b672d160
Hale Xan Yule <551234445@qq.com>
2026-05-15 14:09:38 +0800

    轮播图的搜索框和指示灯

--- lib/page/Home/index.dart
+++ lib/page/Home/index.dart
@@ -15,9 +15,9 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   final List<BannerItem> _bannerList = [
-    BannerItem(id: "1", imgUrl: "https://test.fukit.cn/autoupload/fr/ioCV7UKoxek3BrwKzIT-sAX7Oq-q1b6D22ElINzuG0-yl5f0KlZfm6UsKj-HyTuv/20260510/jD8a/4000X2245/%25E3%2580%2590%25E5%2593%25B2%25E9%25A3%258E%25E5%25A3%2581%25E7%25BA%25B8%25E3%2580%2591%25E5%258F%25AF%25E7%2588%25B1%25E5%25B0%258F%25E7%258B%2597-%25E5%25B0%258F%25E7%258B%2597-%25E6%258A%25A4%25E7%259C%25BC.png"),
-    BannerItem(id: "2", imgUrl: "https://test.fukit.cn/autoupload/fr/ioCV7UKoxek3BrwKzIT-sAX7Oq-q1b6D22ElINzuG0-yl5f0KlZfm6UsKj-HyTuv/20260510/W5vt/1979X1535/%25E3%2580%2590%25E5%2593%25B2%25E9%25A3%258E%25E5%25A3%2581%25E7%25BA%25B8%25E3%2580%2591%25E5%2586%25AC%25E5%25AD%25A3-%25E5%25A3%2581%25E7%25BA%25B8-%25E5%25A4%259C%25E6%2599%259A.png"),
-    BannerItem(id: "3", imgUrl: "https://test.fukit.cn/autoupload/fr/ioCV7UKoxek3BrwKzIT-sAX7Oq-q1b6D22ElINzuG0-yl5f0KlZfm6UsKj-HyTuv/20260510/AXLP/3840X2160/%25E3%2580%2590%25E5%2593%25B2%25E9%25A3%258E%25E5%25A3%2581%25E7%25BA%25B8%25E3%2580%2591%25E4%25BC%2591%25E9%2597%25B2-%25E5%25AE%25A4%25E5%2586%2585-%25E5%25B1%2585%25E5%25AE%25B6.png")
+    BannerItem(id: "1", imgUrl: "https://imgs.catoi.cn/v2/HqcXGGX.jpeg"),
+    BannerItem(id: "2", imgUrl: "https://imgs.catoi.cn/v2/qgTdBMV.jpeg"),
+    BannerItem(id: "3", imgUrl: "https://imgs.catoi.cn/v2/yoxlaeN.jpeg")
   ];
   List<Widget> _getScrollChildren() {
     return [
